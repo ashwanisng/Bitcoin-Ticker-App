3323f380d60f4e42876dc969a667faa4d2b89e43
ashwanisng <ashwanicena5@gmail.com>
2021-01-31 23:40:34 +0530

    appbar created

---
+++ lib/screens/price_screen.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BitCoin Ticker'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
